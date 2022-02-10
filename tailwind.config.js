0960ed8238a9d563dd354e0020477301baae8461
dan.paun <danpaun@deloittece.com>
2022-02-10 16:45:10 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3a22dd',
+      },
+      fontFamily: {
+        nunito: ['Nunito',
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo']
+      }
+    },
+  },
+  plugins: [],
+}
