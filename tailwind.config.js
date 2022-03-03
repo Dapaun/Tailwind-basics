1a26bee219113cf2ed191d5babcd6a87e92e7f8e
Constantin <miconstantin@deloiitece.com>
2022-03-03 11:13:18 +0200

    presentation v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const plugin = require('tailwindcss/plugin')
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  prefix: "dd-",
   theme: {
     extend: {
       colors: {
-        primary: '#3a22dd',
+        primary: '#489fb5',
       },
       fontFamily: {
+        'sans': ["OpenSans", ...defaultTheme.fontFamily.sans],
+        'chronicle-disp': ["ChronicleDisp"],
         nunito: ['Nunito',
         'ui-monospace',
         'SFMono-Regular',
@@ -13,5 +19,21 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl'), color: theme('colors.blue.800'), cursor: theme('cursor.pointer') },
+        '@font-face': {
+          fontFamily: "OpenSans",
+          fontWheight: 400,
+          src: 'url(/src/assets/fonts/OpenSans-Regular.ttf)'
+        },
+        '@font-face': {
+          fontFamily: 'ChronicleDisp',
+          fontWheight: 400,
+          src: 'url(/src/assets/fonts/ChronicleDisp-Black.otf)'
+        }
+      })
+    })
+  ]
 }
